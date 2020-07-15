a9f5c29aeda75e157d8614d63cccc749a9f8f08f
Arto Bendiken <arto@bendiken.net>
2020-07-15 15:09:22 +0300

    Created the header file.

---
+++ dogma.h
@@ -0,0 +1,35 @@
+// This is free and unencumbered software released into the public domain.
+
+#pragma once
+
+/**
+ * @file
+ *
+ * Dogma for C.
+ *
+ * @see https://github.com/dogmatists/dogma.c
+ */
+
+#ifndef __cplusplus
+#if __STDC_VERSION__ < 201103L
+#error "<dogma.h> requires a C11 or newer compiler (CFLAGS='-std=c11')"
+#endif
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h> // for bool
+#include <stddef.h>  // for size_t
+#include <stdint.h>  // for {u,}int*_t
+
+enum {
+  DOGMA_VERSION_MAJOR = 0,
+  DOGMA_VERSION_MINOR = 0,
+  DOGMA_VERSION_PATCH = 0,
+};
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
